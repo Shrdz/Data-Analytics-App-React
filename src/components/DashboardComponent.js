edd7da00f2c0d0db5bbf16bcccb5563f5f40fd80
Shrdz <shraddhayadav146@gmail.com>
2020-12-09 20:24:48 +0530

    Changes with Line chart and dd/mm/yyy dates

--- src/components/DashboardComponent.js
+++ src/components/DashboardComponent.js
@@ -42,7 +42,7 @@ class DashBoard extends Component {
             this.getData(this.state.startDate + '', this.state.endDate + '');
         }
     }
-
+    //fetch token info
     getTokenAndData() {
         let body = { email: "candidate@sigmoid.com", password: "Sigmoid#123", rememberMe: true };
         fetch("https://sigviewauth.sigmoid.io/signIn", {
@@ -103,7 +103,7 @@ class DashBoard extends Component {
     }
 
     getData(startDate, endDate) {
-        let body = { "_id": "Datastory_ChartId_1535224664111", "emailId": "candidate@sigmoid.com", "orgViewReq": { "organization": "DemoTest", "view": "Auction" }, "chartObject": { "metadata": { "title": "", "img_thumbnail": "images/pie.png", "chartType": "pie", "dataLimit": 500 }, "text": [], "requestParam": { "granularity": "hour", "timeZone": { "name": "UTC (+00:00)", "location": "UTC" }, "dateRange": { "startDate": startDate, "endDate": endDate }, "xAxis": ["D005"], "yAxis": [], "approxCountDistinct": [], "specialCalculation": ["CM001"], "filter": [], "orderBy": { "customMetricOrdByList": [{ "id": "CM001", "desc": true }] }, "percentCalList": [{ "id": "CM001" }] } } };
+        let body = { "_id": "Datastory_ChartId_1535224664111", "emailId": "candidate@sigmoid.com", "orgViewReq": { "organization": "DemoTest", "view": "Auction" }, "chartObject": { "metadata": { "title": "", "img_thumbnail": "images/pie.png", "chartType": "pie", "dataLimit": 500 }, "text": [], "requestParam": { "granularity": "hour", "timeZone": { "name": "UTC (+00:00)", "location": "UTC" }, "dateRange": { "startDate": startDate, "endDate": endDate }, "xAxis": ["D002"], "yAxis": [], "approxCountDistinct": [], "specialCalculation": ["CM001"], "filter": [], "orderBy": { "dimOrdByList": [{ "id": "D002", "desc": true }] }, "percentCalList": [{ "id": "CM001" }] } } };
         fetch("https://sigview.sigmoid.io/api/v1/getData", {
             method: "POST",
             headers: {
--- src/components/DoughnutChart.js
+++ src/components/DoughnutChart.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { Doughnut } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 import '../App.css';
 
 function DoughnutChart(props) {
-  let advIdArray = [], dataArray = [];
+  let advIdArray = [], dataArray = [], isoDates = [];
+
   let chartData = props.data;
-  chartData.map(id => advIdArray.push(id.advertiserId));
-  chartData.map(id => dataArray.push(+id.CM001_percent * 100));
+  console.log(chartData)
+  chartData.map(id => advIdArray.push(id.day));
+  chartData.map(id => dataArray.push(+id.CM001 * 100));
 
   //generate random color
   function getRandomColor() {
@@ -18,21 +20,38 @@ function DoughnutChart(props) {
     return color;
   }
 
+  //date in ISO dates
+  advIdArray.sort(); // to display from start date
+  isoDates = advIdArray.map((d) => {
+    d = +d;
+    const monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
+    const dateObj = new Date(d);
+    const month = monthNames[dateObj.getMonth()];
+    const day = String(dateObj.getDate()).padStart(2, '0');
+    const year = dateObj.getFullYear();
+    const output = month + '\n' + day + ',' + year;
+    return output;
+  })
+  // console.log(advIdArray)
+  // console.log(isoDates)
+
   let color = chartData.map(getRandomColor);
   const data = {
-    labels: advIdArray,
+    labels: isoDates,
     datasets: [
       {
-        label: 'Advertisement',
+        label: "First dataset",
         data: dataArray,
-        backgroundColor: color
+        fill: true,
+        backgroundColor: "rgba(75,192,192,0.2)",
+        borderColor: "rgba(75,192,192,1)"
       }
     ]
   }
 
   return (
     <div className="chart">
-      <Doughnut data={data} width={400} height={400} options={{ maintainAspectRatio: false }} />
+      <Line data={data} width={400} height={400} options={{ maintainAspectRatio: false }} />
     </div>
   );
 
